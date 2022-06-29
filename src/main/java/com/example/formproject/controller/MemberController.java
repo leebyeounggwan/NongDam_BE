164e1b0cfa40fba6db56fcb8b4998bf72fede9f5
Lee-Kyung_dong <lkdof27@gmail.com>
2022-06-29 20:31:10 +0900

    회원정보 조회 API 작성, 회원정보 수정 API 작성중 (주석처리)

--- src/main/java/com/example/formproject/controller/MemberController.java
+++ src/main/java/com/example/formproject/controller/MemberController.java
@@ -2,57 +2,79 @@ package com.example.formproject.controller;
 
 import com.example.formproject.dto.request.LoginDto;
 import com.example.formproject.dto.request.MailDto;
+import com.example.formproject.dto.request.MemberInfoRequestDto;
 import com.example.formproject.dto.request.MemberRequestDto;
+import com.example.formproject.entity.Member;
 import com.example.formproject.exception.AuthenticationException;
+import com.example.formproject.repository.MemberRepository;
+import com.example.formproject.security.MemberDetail;
 import com.example.formproject.service.EmailService;
 import com.example.formproject.service.MemberService;
-import com.nimbusds.jose.util.IOUtils;
 import lombok.RequiredArgsConstructor;
 import net.bytebuddy.utility.RandomString;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberRepository repository;
     private final MemberService memberService;
     private final EmailService emailService;
 
+    // 로그인
     @PostMapping("/member/login")
-    public String loginMember(@RequestBody  LoginDto dto, HttpServletResponse response) throws AuthenticationException {
+    public String loginMember(@RequestBody LoginDto dto, HttpServletResponse response) throws AuthenticationException {
         String token = memberService.login(dto);
 
-        response.addHeader("Authorization","Bearer "+token);
-        return "Bearer "+token;
+        response.addHeader("Authorization", "Bearer " + token);
+        return "Bearer " + token;
     }
+
     @PostMapping("/member/email")
     public String emailToken(MailDto dto) throws MessagingException {
         String randomChars = RandomString.make(6);
-        return emailService.sendHtmlEmail(dto,randomChars);
+        return emailService.sendHtmlEmail(dto, randomChars);
     }
+
     @PostMapping("/member/oauth")
-    public void accessTokenToMember(@RequestBody  String t){
-        System.out.println("token : "+t);
-        Map<String,Object> maps = new HashMap<>();
+    public void accessTokenToMember(@RequestBody String t) {
+        System.out.println("token : " + t);
+        Map<String, Object> maps = new HashMap<>();
         OAuth2AccessTokenResponse.withToken(t).tokenType(OAuth2AccessToken.TokenType.BEARER).additionalParameters(maps);
-        for(String key : maps.keySet()){
-            System.out.println(key+":"+maps.get(key));
+        for (String key : maps.keySet()) {
+            System.out.println(key + ":" + maps.get(key));
         }
         System.out.println("end print");
     }
+
     @PostMapping("/member")
     public void joinMember(@RequestBody MemberRequestDto dto) throws IOException {
         memberService.save(dto);
     }
+
+    @GetMapping("/member}")
+    public List<Member> getMember(@AuthenticationPrincipal MemberDetail memberDetail) {
+        return repository.findAllByMember(memberDetail.getMember());
+    }
+
+//    @PutMapping("/member/{memberid}")
+//    public ResponseEntity<?> updateMember(@PathVariable int memberid,
+//                                          @RequestBody MemberInfoRequestDto requestDto,
+//                                          @AuthenticationPrincipal MemberDetail memberDetails) {
+//        String username = memberDetails.getUsername();
+//
+//        return memberService.updateMember(memberid, requestDto, username);
+//    }
 }
---
+++ src/main/java/com/example/formproject/dto/request/MemberInfoRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.formproject.dto.request;
+
+public class MemberInfoRequestDto {
+    private String nickname;
+    private String address;
+    private int contryCode;
+    private int crops;
+    private String profileImage;
+}
