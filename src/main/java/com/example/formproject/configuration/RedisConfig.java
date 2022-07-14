820b47d6bc27787893ed0589662e036ed98bbdb6
BaekGyuHyeon <>
2022-07-14 20:33:54 +0900

    jenkins test

--- src/main/java/com/example/formproject/configuration/RedisConfig.java
+++ src/main/java/com/example/formproject/configuration/RedisConfig.java
@@ -45,6 +45,7 @@ public class RedisConfig {
         RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
     }
