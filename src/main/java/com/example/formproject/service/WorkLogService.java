82d6ca74d0b0bdae7546b3925202423fdd02dc37
Lee-Kyung_dong <lkdof27@gmail.com>
2022-07-26 10:37:01 +0900

    일지 조회 오류 수정

--- src/main/java/com/example/formproject/service/WorkLogService.java
+++ src/main/java/com/example/formproject/service/WorkLogService.java
@@ -110,7 +110,7 @@ public class WorkLogService {
     public List<WorkLogResponseDto> getWorkLogList(MemberDetail detail) throws IllegalArgumentException {
         List<WorkLogResponseDto> responseDtoList = new ArrayList<>();
         List<WorkLog> workLogList = workLogRepository.findAllByMemberOrderByDateDesc(detail.getMember());
-        if (workLogList.get(0) != null) {
+        if (workLogList != null) {
             for (WorkLog log : workLogList)
                 responseDtoList.add(new WorkLogResponseDto(log, new CropDto(log.getCrop())));
             return responseDtoList;
