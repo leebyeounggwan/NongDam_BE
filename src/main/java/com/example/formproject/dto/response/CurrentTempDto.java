64c7dadb9efc3c6087e2907a15e448a2867b16b0
leebyeounggwan <102587669+leebyeounggwan@users.noreply.github.com>
2022-08-02 03:53:07 +0900

    refactor: 날씨 API 수정 CurruntTimeDto 추가

---
+++ src/main/java/com/example/formproject/dto/response/CurrentTempDto.java
@@ -0,0 +1,60 @@
+package com.example.formproject.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+@Getter
+@NoArgsConstructor
+public class CurrentTempDto {
+    // 기온
+    @Schema(type = "String",example = "25")
+    private String temp;
+    //강수량
+    @Schema(type = "String",example = "2")
+    private String rn;
+    //적설량
+    @Schema(type = "String",example = "0")
+    private String sn;
+    //풍속
+    @Schema(type = "String",example = "2.7")
+    private String ws;
+    //습도
+    @Schema(type = "String",example = "87")
+    private String rhm;
+    //날씨
+    @Schema(type = "String",example = "흐림")
+    private String weather;
+    //아이콘 URL
+    @Schema(type = "String",example = "http://idontcare.shop/static/weathericon/01d.png")
+    private String iconURL;
+    //지역
+    @Schema(type = "String",example = "서울시 용산구")
+    private String address;
+    //이슬점
+    @Schema(type = "String",example = "22.3")
+    private String dewPoint;
+
+    public CurrentTempDto(JSONObject obj, String address) {
+        JSONObject parse_response = (JSONObject) obj.get("current");
+        JSONObject snow = (JSONObject) parse_response.get("snow");
+        JSONObject rain = (JSONObject) parse_response.get("rain");
+        JSONArray parse_weather = (JSONArray) parse_response.get("weather");
+        JSONObject value = (JSONObject) parse_weather.get(0);
+        String icon = value.get("icon").toString();
+        String[] strAddr = address.split(" ");
+
+
+        this.temp = parse_response.get("temp").toString().split("\\.")[0];
+        this.rn = (rain == null) ? "0" : rain.get("1h").toString();
+        this.sn = (snow == null) ? "0" : snow.get("1h").toString();
+        this.ws = String.format("%.1f", Double.parseDouble(parse_response.get("wind_speed").toString()));
+        this.rhm = parse_response.get("humidity").toString();
+        this.weather = value.get("description").toString();
+        this.iconURL = "http://idontcare.shop/static/weathericon/"+icon+".png";
+        this.address = strAddr[0]+" "+strAddr[1];
+        this.dewPoint = String.format("%.1f", Double.parseDouble(parse_response.get("dew_point").toString()));
+    }
+}
--- src/main/java/com/example/formproject/dto/response/WeatherResponse.java
+++ src/main/java/com/example/formproject/dto/response/WeatherResponse.java
@@ -1,18 +1,55 @@
 package com.example.formproject.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class WeatherResponse {
+    // 기온
+    @Schema(type = "String",example = "25")
+    private String temp;
+    //강수량
+    @Schema(type = "String",example = "2")
+    private String rn;
+    //적설량
+    @Schema(type = "String",example = "0")
+    private String sn;
+    //풍속
+    @Schema(type = "String",example = "2.7")
+    private String ws;
+    //습도
+    @Schema(type = "String",example = "87")
+    private String rhm;
+    //날씨
+    @Schema(type = "String",example = "흐림")
+    private String weather;
+    //아이콘 URL
+    @Schema(type = "String",example = "http://idontcare.shop/static/weathericon/01d.png")
+    private String iconURL;
+    //지역
+    @Schema(type = "String",example = "서울시 용산구")
+    private String address;
+    //이슬점
+    @Schema(type = "String",example = "22.3")
+    private String dewPoint;
 
-    private final CurrentTempDto currentTempDto;
     //시간별 날씨
-    private final WeatherDto hour;
+    private WeatherDto hour;
     //주간 날씨
-    private final WeatherDto day;
+    private WeatherDto day;
 
     public WeatherResponse(CurrentTempDto currentTempDto, WeatherDto hourlyTemp, WeatherDto dailyTemp) {
-        this.currentTempDto = currentTempDto;
+        this.temp = currentTempDto.getTemp();
+        this.rn = currentTempDto.getRn();
+        this.sn = currentTempDto.getSn();
+        this.ws = currentTempDto.getWs();
+        this.rhm = currentTempDto.getRhm();
+        this.weather = currentTempDto.getWeather();
+        this.iconURL = currentTempDto.getIconURL();
+        this.address = currentTempDto.getAddress();
+        this.dewPoint = currentTempDto.getDewPoint();
         this.hour = hourlyTemp;
         this.day = dailyTemp;
     }
