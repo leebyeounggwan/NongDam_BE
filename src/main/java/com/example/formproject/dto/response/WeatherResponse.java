d7dbde63e1e300bb7ff9840e595a59b472c60a61
leebyeounggwan <102587669+leebyeounggwan@users.noreply.github.com>
2022-08-01 12:12:45 +0900

    refactor: 날씨 API 리팩토링 setter를 생성자로 변경, responseDto 현재날씨를 Dto로 변경, 시간별/주간날씨 함수화

--- src/main/java/com/example/formproject/dto/response/WeatherResponse.java
+++ src/main/java/com/example/formproject/dto/response/WeatherResponse.java
@@ -1,55 +1,20 @@
 package com.example.formproject.dto.response;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter @Setter
-@NoArgsConstructor
+@Getter
 public class WeatherResponse {
 
-
-    // 기온
-    @Schema(type = "String",example = "25")
-    private String temp;
-    //강수량
-    @Schema(type = "String",example = "2")
-    private String rn;
-    //적설량
-    @Schema(type = "String",example = "0")
-    private String sn;
-    //풍속
-    @Schema(type = "String",example = "2.7")
-    private String ws;
-    //습도
-    @Schema(type = "String",example = "87")
-    private String rhm;
-    //날씨
-    @Schema(type = "String",example = "흐림")
-    private String weather;
-    //아이콘 URL
-    @Schema(type = "String",example = "http://idontcare.shop/static/weathericon/01d.png")
-    private String iconURL;
-    //지역
-    @Schema(type = "String",example = "서울시 용산구")
-    private String address;
-    //이슬점
-    @Schema(type = "String",example = "22.3")
-    private String dewPoint;
+    private final CurrentTempDto currentTempDto;
     //시간별 날씨
-    private WeatherDto hour;
+    private final WeatherDto hour;
     //주간 날씨
-    private WeatherDto day;
+    private final WeatherDto day;
 
+    public WeatherResponse(CurrentTempDto currentTempDto, WeatherDto hourlyTemp, WeatherDto dailyTemp) {
+        this.currentTempDto = currentTempDto;
+        this.hour = hourlyTemp;
+        this.day = dailyTemp;
+    }
 }
 
-    /*{
-    기온 / 날씨 / 강수량 / 습도 / 바람 (하루기준)
-        minTamp : 5.5 (최저기온)
-            maxTamp : 20.4 (최고 기온)
-        sumRn : 2 (일 강수량)
-        avgWs : 2 (평균 풍속)
-        avgRhm: 3 (평균 상대 습도)
-        weather : “” (날씨)
-    }*/
--- src/main/java/com/example/formproject/service/OpenWeatherApiService.java
+++ src/main/java/com/example/formproject/service/OpenWeatherApiService.java
@@ -2,6 +2,7 @@ package com.example.formproject.service;
 
 
 import com.example.formproject.annotation.UseCache;
+import com.example.formproject.dto.response.CurrentTempDto;
 import com.example.formproject.dto.response.WeatherDto;
 import com.example.formproject.dto.response.WeatherResponse;
 import com.example.formproject.security.MemberDetail;
@@ -22,120 +23,34 @@ public class OpenWeatherApiService {
     private final GeoService geoService;
     @UseCache(ttl = 0L,cacheKey = "cacheKey",unit = TimeUnit.MINUTES,timeData = false)
     public WeatherResponse getWeather(MemberDetail memberdetail, int cacheKey) throws Exception {
-        WeatherResponse weatherResponse = new WeatherResponse();
 
         String address = (!memberdetail.getMember().getAddress().isEmpty()) ? memberdetail.getMember().getAddress() : "서울 송파구 양재대로 932";
         String[] coords = geoService.getGeoPoint(address);
         StringBuilder apiURL = new StringBuilder("https://api.openweathermap.org/data/2.5/onecall?lat=" + coords[1] + "&lon=" + coords[0] + "&appid=1393bfc76e8aafc98311d5fedf3f59bf&units=metric&lang=kr");
 
-        // API 호출
         JSONObject obj = openApiService.ApiCall(apiURL);
 
-        // 현재 기온을 위한 데이터를 파싱
-        currentTempParse(obj, address, weatherResponse);
+        // 현재 기온
+        CurrentTempDto currentTempDto = currentTempParse(obj, address);
+        // 시간별 기온
+        WeatherDto hourlyTemp = setTempList(obj, "hourly");
+        // 주간 기온
+        WeatherDto dailyTemp = setTempList(obj, "daily");
 
-        // 시간별 기온을 위한 데이터 파싱
-        hourlyTempParse(obj, weatherResponse);
-
-        // 일별 기온을 위한 데이터 파싱
-        dailyTempParse(obj, weatherResponse);
+        WeatherResponse weatherResponse = new WeatherResponse(currentTempDto, hourlyTemp, dailyTemp);
 
         return weatherResponse;
     }
 
 
-    public void currentTempParse(JSONObject obj, String address, WeatherResponse weatherResponse) {
-        JSONObject parse_response = (JSONObject) obj.get("current");
-
-        JSONObject snow = (JSONObject) parse_response.get("snow");
-        if(snow == null){
-            weatherResponse.setSn("0");
-        } else {
-            weatherResponse.setSn(snow.get("1h").toString());
-        }
-        JSONObject rain = (JSONObject) parse_response.get("rain");
-        if(rain == null){
-            weatherResponse.setRn("0");
-        } else {
-            weatherResponse.setRn(rain.get("1h").toString());
-        }
-
-        JSONArray parse_weather = (JSONArray) parse_response.get("weather");
-        JSONObject value = (JSONObject) parse_weather.get(0);
-        String weather = (value.get("description").toString().equals("약간의 구름이 낀 하늘")) ? "구름 낀 하늘" : value.get("description").toString();
-        String icon = value.get("icon").toString();
-        weatherResponse.setTemp(parse_response.get("temp").toString().split("\\.")[0]);
-        weatherResponse.setWs(String.format("%.1f", Double.parseDouble(parse_response.get("wind_speed").toString())));
-        weatherResponse.setRhm(parse_response.get("humidity").toString());
-        weatherResponse.setWeather(weather);
-        weatherResponse.setIconURL("http://idontcare.shop/static/weathericon/"+icon+".png");
-
-        String[] strAddr = address.split(" ");
-        weatherResponse.setAddress(strAddr[0]+" "+strAddr[1]);
-        String dewPoint = String.format("%.1f", Double.parseDouble(parse_response.get("dew_point").toString()));
-        weatherResponse.setDewPoint(dewPoint);
+    public CurrentTempDto currentTempParse(JSONObject obj, String address) {
+        
+        return new CurrentTempDto(obj, address);
     }
 
-    public void hourlyTempParse (JSONObject obj, WeatherResponse weatherResponse) {
-        JSONArray hourlyArr = (JSONArray) obj.get("hourly");
+    public WeatherDto setTempList (JSONObject obj, String select) {
 
-        List<Long> hTimeList = new ArrayList<>();
-        List<String> hTempList = new ArrayList<>();
-        List<String> hPopList = new ArrayList<>();
-        WeatherDto hour = new WeatherDto();
-
-        for(int i=1; i<17; i+=3) {
-            JSONObject hourObj = (JSONObject)hourlyArr.get(i);
-            Long time = (Long) hourObj.get("dt");
-            hTimeList.add(time);
-            hTempList.add(hourObj.get("temp").toString().split("\\.")[0]);
-
-            if (hourObj.get("pop").toString().equals("0")) {
-                hPopList.add("0");
-            } else if (hourObj.get("pop").toString().equals("1")) {
-                hPopList.add("100");
-            }
-            else {
-                double dPop = (double) hourObj.get("pop") * 100;
-                int iPop = (int) dPop;
-                hPopList.add(Integer.toString(iPop));
-            }
-            hour.setTime(hTimeList);
-            hour.setTemp(hTempList);
-            hour.setPop(hPopList);
-        }
-
-        weatherResponse.setHour(hour);
+        return new WeatherDto(obj, select);
     }
 
-    public void dailyTempParse (JSONObject obj, WeatherResponse weatherResponse) {
-        JSONArray dailyArr = (JSONArray) obj.get("daily");
-
-        List<Long> dTimeList = new ArrayList<>();
-        List<String> dTempList = new ArrayList<>();
-        List<String> dPopList = new ArrayList<>();
-        WeatherDto day = new WeatherDto();
-        for(int i=1; i<7; i++) {
-            JSONObject dayObj = (JSONObject) dailyArr.get(i);
-            Long time = (Long) dayObj.get("dt");
-            dTimeList.add(time);
-
-            JSONObject dayTemp = (JSONObject) dayObj.get("temp");
-            dTempList.add(dayTemp.get("day").toString().split("\\.")[0]);
-            if(dayObj.get("pop").toString().equals("0")) {
-                dPopList.add("0");
-            } else if (dayObj.get("pop").toString().equals("1")) {
-                dPopList.add("100");
-            } else {
-                double dPop = (double) dayObj.get("pop") * 100;
-                int iPop = (int) dPop;
-                dPopList.add(Integer.toString(iPop));
-            }
-            day.setTime(dTimeList);
-            day.setTemp(dTempList);
-            day.setPop(dPopList);
-        }
-
-        weatherResponse.setDay(day);
-    }
 }
